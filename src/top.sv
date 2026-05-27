04aebf9c66b71bdba7e1c92bd6b9879710e4850a
DBChrono <Dr.Steve2004@outlook.com>
2026-05-26 17:53:14 -0700

    Starting template

---
+++ src/top.sv
@@ -0,0 +1,41 @@
+`include "src/lcd.sv"
+`include "src/spi.sv"
+module top(
+    input logic CLK,
+
+    // LCD signals
+    output logic LCD_CLK,      // LCD clk
+    output logic LCD_DE,      // Display Enable
+
+    output logic [4:0] LCD_B, // 5-bit blue color data
+    output logic [5:0] LCD_G, // 6-bit green color data
+    output logic [4:0] LCD_R,  // 5-bit red color data
+
+    // SPI signals
+    input  logic SCK,
+    input  logic MOSI,
+    input  logic CS
+);
+
+lcd lcd_dut (
+    .CLK(CLK),
+    .LCD_CLK(LCD_CLK),
+    .LCD_DE(LCD_DE),
+    .LCD_B(LCD_B),
+    .LCD_G(LCD_G),
+    .LCD_R(LCD_R),
+    .pixel(pixel),
+    .pixel_address(pixel_address)
+);
+
+spi spi_dut (
+    .CLK(CLK),
+    .SCK(SCK),
+    .MOSI(MOSI),
+    .CS(CS),
+    .waddr(waddr),
+    .wdata(wdata),
+    .we(we)
+);
+
+endmodule
